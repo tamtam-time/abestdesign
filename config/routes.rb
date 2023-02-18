08f76d36ef35ca82b3902d79bbc590402e69703d
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-02-18 14:48:51 +0900

    コード整理

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'designs#index'
 
-  resources :designs
   resources :designs do
     collection do
       get 'search'
     end
   end
 
-  resources :users
   resources :users, only: :show
   post '/designs/new', to: 'designs#create'
 end
