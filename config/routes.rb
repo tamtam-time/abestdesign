d6dab6065af440f30f68f075f8c8f805ad306935
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-02-19 16:05:12 +0900

    エラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'designs#index'
 
+  resources :designs
   resources :designs do
     collection do
       get 'search'
     end
   end
 
+  resources :users
   resources :users, only: :show
   post '/designs/new', to: 'designs#create'
 end
