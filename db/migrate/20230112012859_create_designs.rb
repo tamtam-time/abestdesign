19bbeca2e718fa9782ec6e291fa49c7f2d7ee2e1
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-02-22 16:11:35 +0900

    マイグレーションファイル修正

--- db/migrate/20230112012859_create_designs.rb
+++ db/migrate/20230112012859_create_designs.rb
@@ -1,8 +1,8 @@
 class CreateDesigns < ActiveRecord::Migration[6.0]
   def change
     create_table :designs do |t|
-      t.string :title
-      t.text :file_name
+      t.string :title, null: false
+      t.text :file_name, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
