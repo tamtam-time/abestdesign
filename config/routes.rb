06648aa15d3b5dcfcf1eb3bc69347bdd3847d8a3
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-03-04 12:25:52 +0900

    検索機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :designs
   resources :designs do
     collection do
-      get 'designs#search'
+      get '/designs/search', to: 'designs#search'
     end
   end
 
